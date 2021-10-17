645de9755e2d8bfdbf494d856dcd2bf063ff4b85
Benjamin Pickford <b.mpickford@outlook.com>
2021-10-17 23:59:02 +1000

    resolve deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import html from '@web/rollup-plugin-html';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'index.html',
   output: { dir: 'docs' },
-  plugins: [html()],
+  plugins: [html(), resolve()],
 };
